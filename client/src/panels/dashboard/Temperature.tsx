d9f3cda363810989ac72f9eaa202602f52c894ed
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-02-04 17:53:28 +0100

    Temperature - partial implementation without proper subrows

--- client/src/panels/dashboard/Temperature.tsx
+++ client/src/panels/dashboard/Temperature.tsx
@@ -4,18 +4,33 @@ import { Widget, WidgetHotbarValue } from "../common/Widget"
 import { GridElement } from "../../common/GridstackGrid/GridstackGrid"
 import type { JSX } from "solid-js/jsx-runtime"
 import { Button } from "../../common/Button/Button"
-import { createSignal, type JSXElement } from "solid-js"
+import { createEffect, createSignal, type JSXElement } from "solid-js"
 import { Icon } from "../../common/Icon/Icon"
 import { ApiFetcher } from "../../common/ApiFetcher/ApiFetcher"
 import { getColor } from "../../common/other/colorGenerator"
 import { LineChart } from "../../common/LineChart/LineChart"
 import { getCountdownArray } from "../../common/other/utils"
+import { countInstancesOfType, getInstancesForType, moduleInstanceColors, useModuleListValue, type Module, type moduleInstancesType } from "../../common/other/ModuleListProvider"
 
-function createRow(data: {icon: string, name: string, rowIndex: number, target: apiMessageSimple, subRowIndex?: number}, index:number) : JSXElement[] {
+type row = {
+    icon: string, 
+    name: string, 
+    target: apiMessageSimple | undefined, 
+    instance: moduleInstancesType
+    subRowIndex?: number,
+}
+
+function createRow(data: row, index:number) : JSXElement[] {
     return [
-        <Icon color={getColor(data.rowIndex)} name={data.icon}></Icon>,
-        <p style={{"justify-content":"start"}}>{data.name}</p>,
-        <ApiFetcher target={data.target} unit="°C"></ApiFetcher>
+        <Icon color={getColor(index)} name={data.icon}></Icon>,
+            <p style={{
+                "justify-content":"start"
+            }}>{data.name}</p>,
+        (data.target)?(
+            <ApiFetcher target={data.target} unit="°C"></ApiFetcher>
+        ):(
+            <p></p>
+        )
     ]
 }
 
@@ -26,43 +41,96 @@ interface TemperatureProps{
 
 export function Temperature(props:TemperatureProps) {
     let i=0;
-    let result = [
-        {
-            icon:       "water_full", 
-            name:       "Bottle", 
-            rowIndex:   i++, 
-            target:     new apiMessageSimple("/sensor/bottle/temperature","temperature")
-        },{
-            icon:       "mode_heat", 
-            name:       "Heater plate", 
-            rowIndex:   i++, 
-            target:     new apiMessageSimple("/control/heater/plate_temperature", "temperature")
-        },{
-            icon:       "wb_incandescent", 
-            name:       "Spectrophotometer", 
-            rowIndex:   i++, 
-            target:     new apiMessageSimple("/sensor/spectrophotometer/emitor/temperature", "temperature")
-        },{
-            icon:       "wb_twilight", 
-            name:       "LED panel", 
-            rowIndex:   i++, 
-            target:     new apiMessageSimple("/control/led_panel/temperature", "temperature")
-        },
+    const [rows, setRows] = createSignal<row[]>([]);
+    const moduleListCntxt = useModuleListValue();
 
-    ]
+    function refreshRows(moduleList: Module[]) {
+        if (moduleListCntxt) {
+            let result: row[] = [];
 
-    for( let i=0; i < Math.random()*100; i++){
-        result.push({
-            icon:       "wb_twilight", 
-            name:       "LED panel", 
-            rowIndex:   i++, 
-            target:     new apiMessageSimple("/control/led_panel/temperature", "temperature")
-        })
+            if (countInstancesOfType(moduleList, "sensor", "Exclusive") > 0) {
+                result.push(
+                    {
+                        icon:       "water_full", 
+                        name:       "Bottle", 
+                        target:     new apiMessageSimple("/sensor/bottle/temperature","temperature"),
+                        instance:   "Exclusive"
+                    },{
+                        icon:       "┣", 
+                        name:       "Top", 
+                        target:     new apiMessageSimple("/sensor/bottle/top/measured_temperature","temperature"),
+                        instance:   "Exclusive"
+                    },{
+                        icon:       "┗", 
+                        name:       "Bottom", 
+                        target:     new apiMessageSimple("/sensor/bottle/bottom/measured_temperature","temperature"),
+                        instance:   "Exclusive"
+                    },{
+                        icon:       "water_lux", 
+                        name:       "Fluorometer", 
+                        target:     undefined,
+                        instance:   "Exclusive"
+                    },{
+                        icon:       "┣", 
+                        name:       "Detector", 
+                        target:     new apiMessageSimple("/sensor/fluorometer/detector/temperature","temperature"),
+                        instance:   "Exclusive"
+                    },{
+                        icon:       "┗", 
+                        name:       "Emitor", 
+                        target:     new apiMessageSimple("/sensor/fluorometer/emitor/temperature","temperature"),
+                        instance:   "Exclusive"
+                    },{
+                        icon:       "wb_incandescent", 
+                        name:       "Spectrophotometer", 
+                        target:     new apiMessageSimple("/sensor/spectrophotometer/emitor/temperature", "temperature"),
+                        instance:   "Exclusive"
+                    },{
+                        icon:       "wb_twilight", 
+                        name:       "LED panel", 
+                        target:     new apiMessageSimple("/control/led_panel/temperature", "temperature"),
+                        instance:   "Exclusive"
+                    },
+                )
+            }
+
+            if(countInstancesOfType(moduleList, "control","Exclusive")>0){
+                result.push(
+                    {
+                        icon:       "mode_heat", 
+                        name:       "Heater plate", 
+                        target:     new apiMessageSimple("/control/heater/plate_temperature", "temperature"),
+                        instance:   "Exclusive"
+                    }
+                )
+            }
+
+            for (let instance of getInstancesForType(moduleList, "pump")) {
+                result.push({
+                    icon: "water_full",
+                    name: "test",
+                    target: new apiMessageSimple("/sensor/bottle/temperature", "temperature"),
+                    instance: instance
+                })
+            }
+
+            setRows(result);
+        }
+    }
+
+    createEffect(()=>{
+        if(moduleListCntxt){
+            refreshRows(moduleListCntxt.state())
+        }
+    })
+
+    if(moduleListCntxt){
+        refreshRows(moduleListCntxt.state())
     }
 
 
     return (
-        <GridElement id={props.id} w={1} h={5 + Math.round(result.length/3)}>
+        <GridElement id={props.id} w={1} h={5 + Math.round(rows().length/3)}>
             <Widget
                 name="Temperature"
                 hotbarTargets={() => {
@@ -80,7 +148,7 @@ export function Temperature(props:TemperatureProps) {
                     )
                 }}
             >
-                <TableStatic renderRow={createRow} data={result} headers={["color", "name", "current"]} colSizes={["40px","100%","60px"]}></TableStatic>
+                <TableStatic renderRow={createRow} data={rows()} headers={["color", "name", "current"]} colSizes={["40px","100%","60px"]}></TableStatic>
                 <div style={{flex:"1 1 auto", "min-height": 0}}>
                     <LineChart labels={getCountdownArray(15)} datasets={[{data:getCountdownArray(15),label:"test"}]}></LineChart>
                 </div>
