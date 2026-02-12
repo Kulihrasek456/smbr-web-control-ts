2c085656f5f31325bcc0bf6af158cd39a84d3497
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-02-12 15:41:29 +0100

    LEDPanel - full implementation

--- client/src/panels/dashboard/LedPanel.tsx
+++ client/src/panels/dashboard/LedPanel.tsx
@@ -1,39 +1,58 @@
 import { createSignal, For, type Accessor, type Setter } from "solid-js";
 import { GridElement } from "../../common/GridstackGrid/GridstackGrid";
-import { Slider } from "../../common/Slider/Slider";
+import { Slider, SliderApiControl } from "../../common/Slider/Slider";
 import { Widget } from "../common/Widget";
 
 import styles from "./LedPanel.module.css"
+import { Button } from "../../common/Button/Button";
+import { sendApiMessage, sendJsonApiMessage } from "../../apiMessages/apiMessageBase";
 
 interface LEDPanelProps{
     id: string;
 }
 
 export function LEDPanel(props: LEDPanelProps){
-    
-    let data : {signal:[Accessor<number>,Setter<number>]}[] = []
-    for(let i=0; i<4; i++){
-        data.push(
-            {
-                signal: createSignal(0)
-            }
-        )
-    }
-
+    const channels = [
+        0,
+        1,
+        2,
+        3
+    ]
     return (
         <GridElement id={props.id} w={1} h={3}>
-            <Widget name="LED panel">
+            <Widget 
+                name="LED panel"
+                hotbarTargets={()=>(
+                    <>
+                        <Button callback={async ()=>{
+                            await sendJsonApiMessage({
+                                url: "/control/led_panel/intensity",
+                                method: "POST",
+                                data: "{\"intensity\": [0,0,0,0]}"
+                            })
+                            return true;
+                        }}>disable</Button>
+                    </>
+                )}
+            >
                 <div class={styles.container}>
-                    <For each={data}>
+                    <For each={channels}>
                         {(el,index)=>(
-                            <Slider
+                            <SliderApiControl
                                 class={styles.slider}
                                 direction="V"
-                                title={"Channel " + index()} 
-                                bounds={{ min: 0, max: 100, show: false }}
-                                getter={el.signal[0]}
-                                setter={el.signal[1]}
-                            ></Slider>
+                                title={"Channel " + el}
+                                bounds={{ min: 0, max: 1, show: false }}
+                                step={0.01}
+                                minInterval={100}
+                                unit="%"
+                                displayModifier={value=>(Math.round(value*100))}
+                                
+                                target={{
+                                    getter:{url:"/control/led_panel/intensity/"+el,key:"intensity"},
+                                    setter:{url:"/control/led_panel/intensity/"+el,key:"intensity"}
+                                }}
+                            ></SliderApiControl>
                         )}
                     </For>
                 </div>
