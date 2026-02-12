f2a3808325257ec6b6c30fc4bba8b49f0a026b6a
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-02-12 15:40:43 +0100

    apiMessages - added simple post message

--- client/src/apiMessages/apiMessageSimple.ts
+++ client/src/apiMessages/apiMessageSimple.ts
@@ -16,9 +16,10 @@ export class ApiMessageSimpleMissingKey extends ApiMessageError {
 export type apiMessageSimpleResult = string | number | boolean
 
 export async function sendApiMessageSimple(options : apiMessageSimple) : Promise<apiMessageSimpleResult>{
-    let opts = {
+    let opts : apiMessageOptions = {
         url: options.url,
-        port: options.port
+        port: options.port,
+        method: "GET"
     }
     let response = await sendJsonApiMessage(opts);
 
@@ -28,4 +29,16 @@ export async function sendApiMessageSimple(options : apiMessageSimple) : Promise
     }else{
         throw new ApiMessageSimpleMissingKey(opts,options.key);
     }
+}
+
+export async function sendApiMessageSimplePost(options : apiMessageSimple, data : any) : Promise<void>{
+    let opts : apiMessageOptions= {
+        url: options.url,
+        port: options.port,
+        method: "POST"
+    }
+
+    opts.data = JSON.stringify({[options.key]: data});
+
+    let response = await sendJsonApiMessage(opts);
 }
