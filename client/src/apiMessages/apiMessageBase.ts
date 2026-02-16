6344856b17a78ff61f9cbb909959ca9ed7a30d70
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-02-16 20:23:27 +0100

    apiMessages - added unified checks for common types

--- client/src/apiMessages/apiMessageBase.ts
+++ client/src/apiMessages/apiMessageBase.ts
@@ -1,3 +1,4 @@
+import { isArray, isBoolean, isNumber, isString } from "../common/other/utils";
 import { smbr_apiMessageConfig } from "./apiMessageConfig";
 
 export interface apiMessageOptions{
@@ -44,7 +45,7 @@ export class ApiUnparsableJsonBody extends ApiMessageError{
 
 export class ApiUnparsableBody extends ApiMessageError{
   constructor(options:apiMessageOptions, message:string){
-    super(options,"unparsable body");
+    super(options,`unparsable body ${message}`);
     this.name = "ApiUnparsableBody";
   }
 }
@@ -99,5 +100,45 @@ export async function sendJsonApiMessage(options:apiMessageOptions) : Promise<ap
       };
     } catch (error) {
       throw new ApiUnparsableJsonBody(options);
+    }
+}
+
+
+export function checkNumber(value: any, key:string, options : apiMessageOptions){
+    if(!isNumber(value[key])){
+        throw new ApiUnparsableBody(options,`response shoud contain a number: ${key}`);
+    }
+}
+
+export function checkArray(value: any, key:string, checkElement: (element : any)=>boolean, options : apiMessageOptions){
+    let values : any[] = value[key]
+
+    if(!isArray(values)){
+        throw new ApiUnparsableBody(options,"should contain samples array")
+    }
+
+    for(let i = 0; i<values.length; i++){
+        if(!checkElement(values[i])){
+          throw new ApiUnparsableBody(options,`element in ${key} is unparsable`)
+        }
+    }
+}
+
+export function checkBoolean(value: any, key:string, options : apiMessageOptions){
+    if(!isBoolean(value[key])){
+        throw new ApiUnparsableBody(options,`response should contain a boolean: ${key}`)
+    }
+}
+
+export function checkString(value: any, key:string, options : apiMessageOptions){
+    if(!isString(value[key])){
+        throw new ApiUnparsableBody(options,`response should contain a string: ${key}`)
+    }
+}
+
+export function checkStringEnum(value: any, key:string, possibleValues: readonly string[], options : apiMessageOptions){
+    checkString(value,key,options);
+    if(!possibleValues.includes((value as string))){
+        throw new ApiUnparsableBody(options,`response should contain an enum: ${key}`)
     }
 }
