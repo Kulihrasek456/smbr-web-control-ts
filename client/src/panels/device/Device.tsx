7e25c4ebbd22fa2b6ec26bb40ea6ff37f3bf2365
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-02-17 18:20:07 +0100

    Device - conditional rendering for device information

--- client/src/panels/device/Device.tsx
+++ client/src/panels/device/Device.tsx
@@ -1,4 +1,6 @@
+import { Show } from "solid-js";
 import { GridstackGrid, GridElement } from "../../common/GridstackGrid/GridstackGrid.tsx";
+import { countInstancesOfType, useModuleListValue } from "../../common/other/ModuleListProvider.tsx";
 import { Control } from "../dashboard/Control.tsx";
 import { Temperature } from "../dashboard/Temperature.tsx";
 import { DeviceInformation } from "./DeviceInformation.tsx";
@@ -6,12 +8,17 @@ import { ModuleListDisplay } from "./ModuleList.tsx";
 import { ServiceStatus } from "./ServiceStatus.tsx";
 
 export function Device() {
+  const moduleListCntxt = useModuleListValue();
+  
+
   return (
     <div style={{ padding: "8px" }}>
       <GridstackGrid>
           <ModuleListDisplay id="moduleList"></ModuleListDisplay>
           <ServiceStatus id="serviceStatus"></ServiceStatus>
-          <DeviceInformation id="deviceInformation"></DeviceInformation>
+          <Show when={countInstancesOfType(moduleListCntxt?.state(),"core","Exclusive")}>
+            <DeviceInformation id="deviceInformation"></DeviceInformation>
+          </Show>
       </GridstackGrid>
     </div>
   );
