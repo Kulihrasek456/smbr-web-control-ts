9d778c42fc08c724bfb7728aab7261d566fdf315
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-02-28 23:26:40 +0100

    Button - show error when action fails

--- client/src/common/Button/Button.tsx
+++ client/src/common/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { children, createSignal, type JSXElement} from 'solid-js'
+import { children, createEffect, createSignal, Show, type JSXElement} from 'solid-js'
 
 import styles from './Button.module.css'
 
@@ -25,27 +25,54 @@ function isDisabled(loading : boolean, disabled : boolean | undefined){
 
 export function Button(props: ButtonProps) {
     const [loading, setLoading] = createSignal<boolean>(false);
+    const [error, setError] = createSignal<boolean>(false);
 
     const onclick = async () => {
         setLoading(true)
+        setError(false);
         try {
-            await props.callback?.();
+            if((await props.callback?.())===false){
+                setError(true);
+            }
         } catch (error) {
+            setError(true);
             console.error(error);
         }
 
         setLoading(false);
     }
 
+    createEffect(()=>{
+        if(error()){
+            setTimeout(()=>{
+                setError(false);
+            },3000);
+        }
+    })
+
 
     return (
         <button 
-            class={`${styles.fetcher} button ${props.class?props.class:""}`} 
+            class={props.class}
+            classList={{
+                [styles.container]: true,
+                [styles.error]: error(),
+                ["button"]:true
+            }} 
             disabled={isDisabled(loading(),props.disabled)}
             onClick={onclick}
             title={getTooltip(props.tooltip,props.disabledTooltip,isDisabled(loading(),props.disabled))}
         >
-            {props.children}
+            <span 
+                class={styles.normal_text}
+            >
+                {props.children}
+            </span>
+            <Show when={error()}>
+                <span
+                    class={styles.error_text}
+                >Err</span>
+            </Show>
         </button>
     );
 }
