f9268ce29da6e2d75861a918d1c2a998c29a26cd
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-02-28 23:26:51 +0100

    Icon - added class prop

--- client/src/common/Icon/Icon.tsx
+++ client/src/common/Icon/Icon.tsx
@@ -4,12 +4,14 @@ type IconProps = {
     name: string;
     filled?: boolean;
     color?: string;
+    class?: string;
 };
 
 export function Icon(props: IconProps) {
 
     return (
         <span
+            class={props.class}
             classList={{
                 [styles.icon]: true,
                 [styles.filled]: props.filled ?? true
