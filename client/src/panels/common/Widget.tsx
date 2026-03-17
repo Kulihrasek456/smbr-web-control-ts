8ba16e1cd75fa0054baf12058aca684ad438a0b4
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-03-17 11:49:53 +0100

    Widget - added unified popup panel

--- client/src/panels/common/Widget.tsx
+++ client/src/panels/common/Widget.tsx
@@ -1,4 +1,4 @@
-import { children, Show } from "solid-js";
+import { children, For, Show } from "solid-js";
 import type { JSX, JSXElement } from "solid-js";
 
 import { Icon } from "../../common/Icon/Icon";
@@ -51,12 +51,14 @@ interface WidgetProps {
     hotbarTargets?: () => JSXElement;
     name: string;
     customRefreshProvider?: boolean;
+    popupPanel?: PopupPanelProps
 }
 
 export function Widget(props: WidgetProps) {
     return (
         <RefreshProviderWrapper wrap={!(props.customRefreshProvider ?? false)}>
             <div class={styles.container}>
+            
                 <div class={styles.header}>
                     <div class={styles["drag-handle"] + " drag-handle"}>
                         <Icon name="open_with"></Icon>
@@ -69,12 +71,69 @@ export function Widget(props: WidgetProps) {
                         <WidgetRefreshButton></WidgetRefreshButton>
                     </div>
                 </div>
-                <div class="popup-container">
-                </div>
                 <div class={styles.body}>
+                    <Show when={props.popupPanel!==undefined}>
+                        <PopupPanel {...(props.popupPanel as PopupPanelProps)}></PopupPanel>
+                    </Show>
                     {props.children}
                 </div>
             </div>
         </RefreshProviderWrapper>
     );
+}
+
+
+export interface Popup {
+  message: string,
+  details: string,
+  severity: "error" | "warning" | "info",
+}
+
+
+export interface PopupPanelProps {
+    getter : () => Array<Popup>
+    setter : (value : Array<Popup>) => void
+    timeout ?: number
+    width ?: string
+    autoHide ?: boolean
+}
+
+export function PopupPanel(props : PopupPanelProps){
+    function removePopup(index : number){
+        let newArr = props.getter();
+        console.error(newArr);
+        newArr = newArr.filter((_,i)=>(i!==index)); 
+        console.error(newArr);
+        props.setter(newArr);
+    }
+
+    return (
+        <div 
+            classList={{
+                [styles.popup_panel]:true,
+                [styles.hidden]:props.getter().length == 0 && (props.autoHide ?? true)
+            }}
+            style={{
+                width: props.width
+            }}
+        >
+            <p class={styles.close_text}>click on a popup to close it</p>
+            <For each={props.getter()}>
+                {(el,index)=>(
+                    <button 
+                        classList={{
+                            [styles.popup]:true,
+                            [styles.info]:el.severity == "info",
+                            [styles.warning]:el.severity == "warning",
+                            [styles.error]:el.severity == "error"
+                        }}
+                        onclick={e=>removePopup(index())}
+                    >
+                        <p class={styles.error_text}>{el.message}</p>
+                        <p class={styles.message_text}>{el.details}</p>
+                    </button>
+                )}
+            </For>
+        </div>
+    )
 }
