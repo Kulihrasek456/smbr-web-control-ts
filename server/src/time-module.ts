b303fcca2a7e2e873dee2a919cddb5e2b675d8a4
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-04-07 16:34:22 +0200

    Server - added /time/convert endpoint

--- server/src/time-module.ts
+++ server/src/time-module.ts
@@ -3,6 +3,7 @@ import { Router, type Request, type Response } from "express";
 export const timeModulesRouter = Router();
 
 timeModulesRouter.get("/",getTime)
+timeModulesRouter.post("/convert",convertTime)
 
 function getTime(req: Request, res: Response){
     res.send(JSON.stringify(
@@ -10,4 +11,25 @@ function getTime(req: Request, res: Response){
             serverTime: new Date().toUTCString()
         }
     ));
+}
+
+/*
+Converts from the Rest APIs stripped timstamp 
+(with applied time zone and no information about it)
+to a valid UTC timestamp
+*/
+function convertTime(req: Request, res: Response){
+    if(req.body.time === undefined){
+        res.status(400).send(JSON.stringify({message:"missing values in message body"}));
+    }
+    try{
+        let result = new Date(req.body.time+"Z").getTime()
+        res.send(JSON.stringify(
+            {
+                convertedTime: result
+            }
+        ));
+    }catch{
+        res.status(400).send(JSON.stringify({message:"invalid timestamp"}));
+    }
 }
