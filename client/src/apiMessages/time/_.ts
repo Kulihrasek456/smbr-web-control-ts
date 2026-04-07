691aec8614580fbcc97d3f86ac9c76426bd947aa
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-04-07 16:34:50 +0200

    apiMessages - added /time/convert endpoint support

--- client/src/apiMessages/time/_.ts
+++ client/src/apiMessages/time/_.ts
@@ -1,4 +1,4 @@
-import { checkTimestamp, sendJsonApiMessage, type apiMessageOptions } from "../apiMessageBase";
+import { checkNumber, checkTimestamp, sendJsonApiMessage, type apiMessageOptions } from "../apiMessageBase";
 
 export namespace Time{
     export async function getTime() : Promise<Date>{
@@ -19,5 +19,27 @@ export namespace Time{
         let roundTripTime = Date.now() - sendTime;
 
         return new Date(responseTime.getTime() - (roundTripTime/2))
+    }
+
+    export type convertTime = {
+        timestamp : string
+    }
+
+    export async function sendConvertTime(options : convertTime) : Promise<Date>{
+        let opts : apiMessageOptions = {
+            url: "/time/convert",
+            target: "webControlApi",
+            method: "POST",
+            data: JSON.stringify({
+                time: options.timestamp
+            })
+        }
+
+        let result = await sendJsonApiMessage(opts);
+        let data = result.jsonValue;
+
+        checkNumber(data,"convertedTime",opts);
+
+        return new Date(data.convertedTime);
     }
 }
