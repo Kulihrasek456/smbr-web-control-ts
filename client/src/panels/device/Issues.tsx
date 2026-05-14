3c929541e8900f42f07f7c80bbfbaf8127bb8507
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-05-14 14:54:18 +0200

    Issues - added per type rendering

--- client/src/panels/device/Issues.tsx
+++ client/src/panels/device/Issues.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal, For, Show } from "solid-js"
+import { createEffect, createSignal, For, Show, type JSXElement } from "solid-js"
 import { GridElement } from "../../common/GridstackGrid/GridstackGrid"
 import { Widget } from "../common/Widget"
 import styles from "./Issues.module.css"
@@ -6,21 +6,131 @@ import { System } from "../../apiMessages/system/_";
 import { refreshValueUpdate, useRefreshContext } from "../../common/other/RefreshProvider";
 import { ValueDisplay } from "../../common/ApiFetcher/ValueDisplay";
 import { TableStatic } from "../../common/Table/Table";
-import { moduleInstanceColors } from "../../common/other/ModuleListProvider";
+import { moduleInstanceColors, moduleInstances } from "../../common/other/ModuleListProvider";
+import { render } from "solid-js/web";
 
 
-function renderRow(el : System.issueType, index : number){
-    return [
-        <p>{el.timestamp}</p>,
-        <p>{el.name}</p>,
-        <p>{el.index}</p>,
-        <p>{el.value.toFixed(2)}</p>,
-        <p>{el.module}</p>,
-        <p>{el.instance}</p>,
-        <p>{el.id}</p>
-    ]
+function renderGeneric(issue: System.issueType) : JSXElement {
+    return renderSpecific(
+        issue,
+        <>
+            <p class={styles.name}>{issue.name}</p>
+            <div class={styles.labeled_value}>
+                <p>Value:</p>
+                <ValueDisplay value={issue.value.toFixed(2)}></ValueDisplay>
+            </div>
+        </>,
+        issue.index.toFixed(0)     
+    )
 }
 
+function renderSpecific(issue: System.issueType, message: JSXElement, index: string | undefined = undefined): JSXElement {
+    let timestampSplit = issue.timestamp.split("T");
+    return (
+        <div class={styles.issue} style={{"--instance-color":moduleInstanceColors[issue.instance]}}>
+            <div class={styles.basic_info}>
+                <div class={styles.basic_info}>
+                    <div class={styles.labeled_value}>
+                    <p>module:</p>
+                    <p class={styles.module_name}>{issue.module} ({issue.instance})</p>
+                    </div>
+                    <Show when={index !== undefined}>
+                        <div class={styles.labeled_value}>
+                            <p>Index:</p>
+                            <p>{index}</p>
+                        </div>
+                    </Show>
+                </div>
+                <div class={styles.time}>
+                    <p>{timestampSplit[0]}</p>
+                    <p>{timestampSplit[1]}</p>
+                </div>
+            </div>
+            <div class={styles.values}>
+                <p class={styles.id}>{issue.id}</p>
+                <div class={styles.message}>
+                    {message}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function renderTemperatureIssue(issue: System.issueType, tempTarget: string, index?: string): JSXElement {
+    return renderSpecific(
+        issue,
+        <><p><b>{tempTarget}</b> temperature is too high (</p><ValueDisplay value={issue.value.toFixed(1)} unit="°C"></ValueDisplay><p>)</p></>
+    );
+}
+
+function renderIssue(issue: System.issueType) : JSXElement{
+    switch (issue.name) {
+        case "HighLoad":
+            return renderSpecific(
+                issue,
+                <><p><b>CPU</b> usage has reached a high threshold (</p><ValueDisplay value={issue.value.toFixed(1)} unit="%"></ValueDisplay><p>)</p></>
+            );
+        case "CoreOverTemp":
+            return renderTemperatureIssue(issue,"CPU");
+            
+        case "BoardOverTemp":
+            return renderTemperatureIssue(issue,"Board");
+        case "Invalid5VSupply":
+            return renderSpecific(
+                issue,
+                <><p><b>5V</b> supply is invalid</p></> //#TODO
+            );
+        case "InvalidVinSupply":
+            return renderSpecific(
+                issue,
+                <><p><b>Vin</b> supply is invalid</p></> //#TODO
+            );
+        case "InvalidPoESupply":
+            return renderSpecific(
+                issue,
+                <><p><b>PoE</b> supply is invalid</p></> //#TODO
+            );
+        case "OverCurrentDraw":
+            return renderSpecific(
+                issue,
+                <><p><b>Current</b> draw over threshold: (</p><ValueDisplay value={issue.value.toFixed(1)} unit="A"></ValueDisplay><p>)</p></>
+            );
+        case "OverPowerDraw":
+            return renderSpecific(
+                issue,
+                <><p><b>Power</b> draw over threshold: (</p><ValueDisplay value={issue.value.toFixed(1)} unit="A"></ValueDisplay><p>)</p></>
+            );
+        case "LEDPanelOverTemp":
+            return renderTemperatureIssue(issue,"LED panel");
+        case "HeaterOverTemp":
+            return renderTemperatureIssue(issue,"Heater");
+        case "MixerOverRPM":
+            return renderTemperatureIssue(issue, "Mixer");
+        case "BottleOverTemp":
+            return renderTemperatureIssue(issue,"Bottle");
+        case "BottleTopOverMeasTemp":
+            return renderTemperatureIssue(issue,"Bottle (top)");
+        case "BottleBottomOverMeasTemp":
+            return renderTemperatureIssue(issue,"Bottle (bottom)");
+        case "BottleTopOverSensorTemp":
+            return renderTemperatureIssue(issue,"Bottle sensor (top)");
+        case "BottleBottomOverSensorTemp":
+            return renderTemperatureIssue(issue, "Bottle sensor (bottom)");
+        case "FluorometerDetectorOverTemp":
+            return renderTemperatureIssue(issue,"Fluorometer detector");
+        case "FluorometerEmitorOverTemp":
+            return renderTemperatureIssue(issue,"Fluorometer emitor");
+        case "SpectrophotometerEmitorOverTemp":
+            return renderTemperatureIssue(issue, "Spectroph... emitor");
+        case "InvalidInstance": //#TODO not tested as it is not in the API (yet)
+            return renderSpecific(
+                issue,
+                <><p>Invalid instance selected (</p><ValueDisplay value={moduleInstances[issue.index]}></ValueDisplay><p>)!</p></>
+            )
+        default:
+            return renderGeneric(issue);
+    }
+}
 
 interface ModuleIssuesProps {
     id: string
@@ -60,30 +170,7 @@ function ModuleIssuesBody(props : ModuleIssuesProps){
                 <div class={styles.issue_list}>
                     <For each={issues() ?? []}>
                         {(el,index)=>(
-                            <div class={styles.issue} style={{"--instance-color":moduleInstanceColors[el.instance]}}>
-                                <div class={styles.basic_info}>
-                                    <div class={styles.basic_info}>
-                                        <div class={styles.labeled_value}>
-                                        <p>module:</p>
-                                        <p class={styles.module_name}>{el.module} ({el.instance})</p>
-                                        </div>
-                                        <div class={styles.labeled_value}>
-                                            <p>Index:</p>
-                                            <p>{el.index.toFixed(0)}</p>
-                                        </div>
-                                    </div>
-                                    
-                                    <ValueDisplay class={styles.time} value={el.timestamp.split("T")[1]}></ValueDisplay>
-                                </div>
-                                <div class={styles.values}>
-                                    <p class={styles.id}>{el.id}</p>
-                                    <p class={styles.name}>{el.name}</p>
-                                    <div class={styles.labeled_value}>
-                                        <p>Value:</p>
-                                        <ValueDisplay value={el.value.toFixed(2)}></ValueDisplay>
-                                    </div>
-                                </div>
-                            </div>
+                            renderIssue(el)
                         )}
                     </For>
                 </div>
